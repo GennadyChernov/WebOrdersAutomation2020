55784806d003f08861f7b5cd44569208f5c2a4c6
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-04-05 16:28:48 -0400

    update

--- src/test/java/com/weborders/test/AbstractBaseTest.java
+++ src/test/java/com/weborders/test/AbstractBaseTest.java
@@ -1,5 +1,8 @@
 package com.weborders.test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.weborders.utilities.BrowserUtilities;
 import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.Driver;
@@ -7,12 +10,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import javax.security.auth.login.Configuration;
+import java.io.IOException;
 
 public abstract class AbstractBaseTest {
     protected WebDriver driver = Driver.getDriver();
+    protected ExtentReports extentReports;
+    protected ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+
+    @BeforeTest
+    public void beforeTest() {
+        extentReports = new ExtentReports();
+        String reportPath = "";
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/report.html";
+        }
+    }
+
+    @AfterTest
+    public void afterTest() {
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup() {
@@ -24,9 +50,21 @@ public abstract class AbstractBaseTest {
     @AfterMethod
     public void tearDown(ITestResult testResult) {
         if (testResult.getStatus() == ITestResult.FAILURE) {
-         String screenshotLocation =  BrowserUtilities.getScreenshot(testResult.getName());
+            String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
+            try {
+                extentTest.fail(testResult.getName());
+                extentTest.addScreenCaptureFromPath(screenshotLocation);
+                extentTest.fail(testResult.getThrowable());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to attach screenshot");
+            }
+        } else if (testResult.getStatus() == ITestResult.SUCCESS) {
+            extentTest.pass(testResult.getName());
+        } else if (testResult.getStatus() == ITestResult.SKIP) {
+            extentTest.skip(testResult.getName());
         }
-
+        BrowserUtilities.wait(3);
         Driver.closeDriver();
     }
 
