dd64fc5e4b506bc90c0a3428a85a64aa7888959a
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-04-05 15:20:23 -0400

    update

--- src/test/java/com/weborders/test/AbstractBaseTest.java
+++ src/test/java/com/weborders/test/AbstractBaseTest.java
@@ -1,5 +1,34 @@
 package com.weborders.test;
 
-public class AbstractBaseTest {
+import com.weborders.utilities.BrowserUtilities;
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import javax.security.auth.login.Configuration;
+
+public abstract class AbstractBaseTest {
+    protected WebDriver driver = Driver.getDriver();
+
+    @BeforeMethod
+    public void setup() {
+        driver.get(ConfigurationReader.getProperty("url"));
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterMethod
+    public void tearDown(ITestResult testResult) {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+         String screenshotLocation =  BrowserUtilities.getScreenshot(testResult.getName());
+        }
+
+        Driver.closeDriver();
+    }
+
 }
 
